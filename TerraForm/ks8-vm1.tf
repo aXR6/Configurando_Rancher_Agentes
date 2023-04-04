d9b76529dd17532e72b4e79a6ab4f13e8cde2d51
root <thalles_tsc@hotmail.com>
2023-04-04 12:58:46 -0300

    Inseridos arquivos de configuração TerraForm

---
+++ TerraForm/ks8-vm1.tf
@@ -0,0 +1,36 @@
+# Provider Proxmox
+provider "proxmox" {
+  pm_api_url   = "https://proxmox-ks8.ddns.net:8006/api2/json"
+  pm_user_name = "root"
+  pm_password  = "902grego1989"
+  pm_tls_insecure = true
+}
+
+# Resource de configuração da máquina virtual
+resource "proxmox_vm_qemu" "ks8-vm1" {
+  name = "vm-debian-11"
+  target_node = "pvedell5437"
+
+  # Configurações de memória RAM e SWAP
+  memory = "3584"
+  swap   = "2048"
+
+  # Configurações de disco
+  disk_size = "40G"
+  storage   = "Dell5437"
+
+  # Configurações do sistema operacional
+  os_type = "debian"
+  os_variant = "debian11"
+  
+  # Configuração de rede
+  network {
+    model = "virtio"
+  }
+}
+
+# Recurso para a rede virtual
+resource "proxmox_network" "example" {
+  id   = "vmbr0"
+  vlan = "0"
+}
