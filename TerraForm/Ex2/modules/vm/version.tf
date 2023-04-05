f8efcc2d0a059853d26c465038aa0aaf84e75eaf
root <thalles_tsc@hotmail.com>
2023-04-05 17:29:30 -0300

    Arquivos configurados

--- TerraForm/Ex2/modules/vm/version.tf
+++ TerraForm/Ex2/modules/vm/version.tf
@@ -11,14 +11,17 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
 provider "proxmox" {
   pm_api_url          = "https://192.168.2.50:8006/api2/json"
-  pm_user             = "root"
-  pm_password         = "902grego1989"
-  pm_tls_insecure     = true
-  pm_debug            = true
-  #pm_api_token_id     = "root"
-  #pm_api_token_secret = "902grego1989"
+ 
+  #pm_user             = "root"
+  #pm_password         = "902grego1989"
   #pm_tls_insecure     = true
   #pm_debug            = true
+
+  pm_api_token_id     = "root"
+  pm_api_token_secret = "902grego1989"
+  pm_tls_insecure     = true
+  pm_debug            = true
 }
