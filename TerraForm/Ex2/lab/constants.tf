cb4af77883138d6f6e9fb9357f587d2c5a935f9b
root <thalles_tsc@hotmail.com>
2023-04-06 17:49:37 -0300

    Alteração nos arquivos

--- TerraForm/Ex2/lab/constants.tf
+++ TerraForm/Ex2/lab/constants.tf
@@ -81,6 +81,33 @@ locals {
         network_firewall    = false #defaults to false
         dns_servers         = "192.168.2.200 192.168.2.201 8.8.8.8"
       }
+
+      m4 = {
+        name                = "ks8-rancher"
+        target_node         = "pvedell5437" # Name of the Proxmox Server
+        qemu_os             = "Linux"       # Type of Operating System
+        os_type             = "cloud-init"  # Set to cloud-init to utilize templates
+        agent               = 1             # Set to 1 to enable the QEMU Guest Agent. Note, you must run the qemu-guest-agent daemon in the guest for this to have any effect.
+        full_clone          = true          # Set to true to create a full clone, or false to create a linked clone. See the docs about cloning for more info. Only applies when clone is set.
+        template            = "Debian11CloudInit"      # Name of Template Used to Clone
+        cores               = 2
+        socket              = 1
+        memory              = 3500
+        storage             = "35G"         # Size of Secondary hard drive assiged as bootable
+        ip_address          = "192.168.2.10"
+        gateway             = "192.168.2.254"
+        description         = "Máquina virtual - ks8-rancher."
+        ssh_user            = "ks8rancher"
+        mac_address         = "22:E2:A0:2F:92:53"
+        disk_type           = "virtio"
+        storage_dev         = "local-lvm"
+        network_bridge_type = "vmbr0"
+        network_model       = "virtio"
+        cloud_init_pass     = "ks8rancher"
+        automatic_reboot    = true
+        network_firewall    = false #defaults to false
+        dns_servers         = "192.168.2.200 192.168.2.201 8.8.8.8"
+      }
     }
   }
 
