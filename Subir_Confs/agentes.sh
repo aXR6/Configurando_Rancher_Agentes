8f2e404a9c4df93633d9a989bde7cc01a8e0f43e
root <thalles_tsc@hotmail.com>
2023-04-06 11:47:26 -0300

    Alteração nos arquivos

--- Subir_Confs/agentes.sh
+++ Subir_Confs/agentes.sh
@@ -42,65 +42,6 @@ EOT
 
 echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
 
-echo -e "\033[1;31m:=> Criando o arquivo: AUTOUPDATE \033[0m"
-echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
-touch /bin/autoupdate
-chmod 777 /bin/autoupdate
-cat >'/bin/autoupdate' <<EOT
-#!/bin/bash
-
-# Função que atualiza a distribuição Debian
-update_debian() {
-  apt-get update
-  apt-get upgrade -y
-  apt-get autoremove -y
-  apt-get autoclean
-  apt-get clean
-}
-
-# Função que atualiza a distribuição Debian sem atualizar o Docker
-update_debian_without_docker() {
-  apt-mark hold docker-ce docker-ce-cli containerd.io docker-buildx-plugin
-  apt-get update
-  apt-get upgrade -y
-  apt-get autoremove -y
-  apt-get autoclean
-  apt-get clean
-  apt-mark unhold docker-ce docker-ce-cli containerd.io docker-buildx-plugin
-}
-
-# Verifica se o usuário deseja atualizar a distribuição sem atualizar o Docker
-if [ "$1" == "--without-docker" ]; then
-  update_debian_without_docker
-else
-  update_debian
-fi
-EOT
-echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
-
-echo -e "\033[1;31m:=> Configurando o serviço que iniciará o AUTOUPDATE \033[0m"
-echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
-touch /lib/systemd/system/updateserv.service
-chmod 777 /lib/systemd/system/updateserv.service
-
-cat >'/lib/systemd/system/updateserv.service' <<EOT
-[Unit]
-Description=Atualiza a distribuição Linux
-
-[Service]
-Type=simple
-ExecStart=/bin/bash /bin/autoupdate --without-docker
-
-#./autoupdate                  # atualiza a distribuição normalmente
-#./autoupdate --without-docker # atualiza a distribuição sem atualizar o Docker
-
-
-[Install]
-WantedBy=multi-user.target
-EOT
-
-echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
-
 echo -e "\033[1;31m:=> Preparando o ambiente e instalando o Docker \033[0m"
 echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
 curl https://releases.rancher.com/install-docker/20.10.sh | sh && usermod -aG docker root
@@ -140,10 +81,7 @@ echo -e "\033[1;31m:=> Startando serviços recem criados \033[0m"
 echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
 systemctl daemon-reload
 
-systemctl enable updateserv.service
 systemctl enable dns.service
-
-systemctl start updateserv.service
 systemctl start dns.service
 echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
 
