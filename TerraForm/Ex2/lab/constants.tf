cd2c08f7c776d7d511fa019664e6f8a4aa7cdb9e
root <thalles_tsc@hotmail.com>
2023-04-06 12:58:24 -0300

    Alteração nos arquivos

--- TerraForm/Ex2/lab/constants.tf
+++ TerraForm/Ex2/lab/constants.tf
@@ -9,6 +9,7 @@ locals {
         agent               = 1             # Set to 1 to enable the QEMU Guest Agent. Note, you must run the qemu-guest-agent daemon in the guest for this to have any effect.
         full_clone          = true          # Set to true to create a full clone, or false to create a linked clone. See the docs about cloning for more info. Only applies when clone is set.
         template_vm_id      = "101"         # Name of Template Used to Clone
+        template_storage    = "local-lvm"
         cores               = 2
         socket              = 1
         memory              = 3530
