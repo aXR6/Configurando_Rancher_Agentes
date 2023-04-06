75d7757ad61f3d6a1eddecd8da204dc31ae2280a
root <thalles_tsc@hotmail.com>
2023-04-06 10:59:23 -0300

    Alteração nos arquivos

--- TerraForm/Ex2/lab/constants.tf
+++ TerraForm/Ex2/lab/constants.tf
@@ -4,28 +4,28 @@ locals {
       m1 = {
         name                = "talos-dns"
         target_node         = "pvedell5437" # Name of the Proxmox Server
-        qemu_os             = "other" # Type of Operating System
-        os_type             = "cloud-init" # Set to cloud-init to utilize templates
-        agent               = 1           # Set to 1 to enable the QEMU Guest Agent. Note, you must run the qemu-guest-agent daemon in the guest for this to have any effect.
-        full_clone          = true        # Set to true to create a full clone, or false to create a linked clone. See the docs about cloning for more info. Only applies when clone is set.
-        template            = "lunar-tpl" # Name of Template Used to Clone
-        cores               = 4
+        qemu_os             = "linux"       # Type of Operating System
+        os_type             = "cloud-init"  # Set to cloud-init to utilize templates
+        agent               = 1             # Set to 1 to enable the QEMU Guest Agent. Note, you must run the qemu-guest-agent daemon in the guest for this to have any effect.
+        full_clone          = true          # Set to true to create a full clone, or false to create a linked clone. See the docs about cloning for more info. Only applies when clone is set.
+        template            = "lunar-tpl"   # Name of Template Used to Clone
+        cores               = 2
         socket              = 1
-        memory              = 6144
-        storage             = "40G" # Size of Secondary hard drive assiged as bootable
-        ip_address          = "10.0.125.110"
-        gateway             = "10.0.125.1"
-        description         = "Load Balancer"
-        ssh_user            = "haproxyadmin"
-        mac_address         = "02:07:14:b4:37:24"
+        memory              = 3530
+        storage             = "40G"         # Size of Secondary hard drive assiged as bootable
+        ip_address          = "192.168.2.11"
+        gateway             = "192.168.2.254"
+        description         = "Máquina virtual - ks8-vm1 - para Rancher."
+        ssh_user            = "ks8vm1"
+        mac_address         = "22:E2:A0:2F:92:53"
         disk_type           = "virtio"
         storage_dev         = "groot"
         network_bridge_type = "vmbr0"
         network_model       = "virtio"
-        cloud_init_pass     = "902grego1989"
+        cloud_init_pass     = "ks8vm1"
         automatic_reboot    = true
         network_firewall    = false #defaults to false
-        dns_servers         = "10.0.125.1 8.8.8.8 10.0.125.1 127.0.0.1"
+        dns_servers         = "192.168.2.200 192.168.2.201 8.8.8.8"
       }
     }
   }
