eebc93300c1bbe91d0f6960ebff8db9ee6e05971
root <thalles_tsc@hotmail.com>
2023-04-09 15:28:50 -0300

    Atualização

--- Subir_Confs/padrao.sh
+++ Subir_Confs/padrao.sh
@@ -43,33 +43,11 @@ sudo touch /bin/autoupdate
 sudo chmod 777 /bin/autoupdate
 sudo cat >'/bin/autoupdate' <<EOT
 #!/bin/bash
-
-# Função que atualiza a distribuição Debian
-update_debian() {
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get autoremove -y
-  sudo apt-get autoclean
-  sudo apt-get clean
-}
-
-# Função que atualiza a distribuição Debian sem atualizar o Docker
-update_debian_without_docker() {
-  sudo apt-mark hold docker-ce docker-ce-cli containerd.io docker-buildx-plugin
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get autoremove -y
-  sudo apt-get autoclean
-  sudo apt-get clean
-  sudo apt-mark unhold docker-ce docker-ce-cli containerd.io docker-buildx-plugin
-}
-
-# Verifica se o usuário deseja atualizar a distribuição sem atualizar o Docker
-if [ "$1" == "--without-docker" ]; then
-  update_debian_without_docker
-else
-  update_debian
-fi
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean
+sudo apt-get clean
 EOT
 echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
 
@@ -84,18 +62,20 @@ Description=Atualiza a distribuição Linux
 
 [Service]
 Type=simple
-ExecStart=/bin/bash /bin/autoupdate --without-docker
+ExecStart=/bin/bash /bin/autoupdate
 
 #./autoupdate                  # atualiza a distribuição normalmente
-#./autoupdate --without-docker # atualiza a distribuição sem atualizar o Docker
+#./autoupdate                  # atualiza a distribuição sem atualizar o Docker
 
 
 [Install]
 WantedBy=multi-user.target
 EOT
-echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
-sudo systemctl enable updateserv.service &&
-sudo systemctl enable dns.service &&
 
-sudo systemctl start updateserv.service &&
+echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
+sudo systemctl enable updateserv.service
+sudo systemctl enable dns.service
+
+sudo systemctl start updateserv.service
 sudo systemctl start dns.service
+echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
