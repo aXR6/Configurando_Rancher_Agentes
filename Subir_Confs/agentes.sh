4aa0019552ba963f0757ebbb82beb1d1a690d85b
root <thalles_tsc@hotmail.com>
2023-04-19 17:55:04 -0300

    Atualização

--- Subir_Confs/agentes.sh
+++ Subir_Confs/agentes.sh
@@ -134,3 +134,84 @@ echo -e "\033[1;31m:=>----------------------------------------------------------
 sudo docker container ls
 sudo docker --version
 
+echo -e "\033[1;31m:=> Configurando o CSF Firewall \033[0m"
+echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
+sudo apt install -y perl zip unzip libwww-perl liblwp-protocol-https-perl wget
+sudo apt install -y sendmail-bin
+
+cd /usr/src
+sudo wget https://download.configserver.com/csf.tgz
+sudo tar -xzvf csf.tgz
+cd csf 
+sudo sh install.sh
+
+sudo perl /usr/local/csf/bin/csftest.pl
+
+sudo cat >'/etc/csf/csf.conf' <<EOT
+# Configurações de Firewall csf
+
+# Habilitar o csf
+TESTING = "0"
+# Alterar para "1" para executar no modo de teste sem bloquear
+# Alterar para "0" para executar no modo de produção
+
+# Lista de portas TCP permitidas
+TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"
+TCP_OUT = "20,21,22,25,53,80,110,113,443"
+
+# Lista de portas UDP permitidas
+UDP_IN = "53"
+UDP_OUT = "20,21,53"
+
+# Proteger contra ataques SYN
+SYN_ATTACK = "1"
+
+# Proteger contra ataques de portas aleatórias
+PORTFLOOD = "80;tcp;20;5"
+
+# Proteger contra ataque DDoS
+DDOS = "1"
+
+# Proteger contra ataques de bruteforce SSH
+LF_SSHD = "5"
+
+# Proteger contra ataques de bruteforce FTP
+LF_FTPD = "5"
+
+# Proteger contra ataques de bruteforce POP3
+LF_POP3D = "5"
+
+# Proteger contra ataques de bruteforce IMAP
+LF_IMAPD = "5"
+
+# Proteger contra ataques de bruteforce SFTP
+LF_SFTPD = "5"
+
+# Proteger contra ataques de bruteforce Exim
+LF_EXIMSYNTAX = "5"
+
+# Proteger contra ataques de bruteforce cPanel
+LF_CPANEL = "5"
+
+# Proteger contra ataques de bruteforce WHM
+LF_CPANEL = "5"
+
+# Bloquear IPs mal-intencionados
+DROP_IPS = "1"
+
+# Proteger contra o uso malicioso de executáveis PHP
+PHP_FPM = "1"
+
+# Permitir conexões SSL
+CT_LIMIT = "80;300;5/300;6"
+EOT
+
+sudo csf -a 192.168.2.10
+sudo csf -a 192.168.2.11
+sudo csf -a 192.168.2.12
+sudo csf -a 192.168.2.13
+sudo csf -a 192.168.2.200
+sudo csf -a 192.168.2.201
+sudo csf -a 192.168.2.203
+
+sudo csf -s
--- Subir_Confs/padrao.sh
+++ Subir_Confs/padrao.sh
@@ -112,32 +112,13 @@ SYN_ATTACK = "1"
 # Proteger contra ataques de portas aleatórias
 PORTFLOOD = "80;tcp;20;5"
 
-# Proecho -e "\033[1;31m:=> Configurando o CSF Firewall \033[0m"
-echo -e "\033[1;31m:=>---------------------------------------------------------------------------------------------------------------------------\033[0m"
-sudo apt install -y perl zip unzip libwww-perl liblwp-protocol-https-perl wget
-sudo apt install -y sendmail-bin
+# Proteger contra ataque DDoS
+DDOS = "1"
 
-cd /usr/src
-sudo wget https://download.configserver.com/csf.tgz
-sudo tar -xzvf csf.tgz
-cd csf 
-sudo sh install.sh
+# Proteger contra ataques de bruteforce SSH
+LF_SSHD = "5"
 
-sudo perl /usr/local/csf/bin/csftest.pl
-
-sudo cat >'/etc/csf/csf.conf' <<EOT
-# Configurações de Firewall csf
-
-# Habilitar o csf
-TESTING = "0"
-# Alterar para "1" para executar no modo de teste sem bloquear
-# Alterar para "0" para executar no modo de produção
-
-# Lista de portas TCP permitidas
-TCP_IN = "20,21,22,25,53,80,110,143,443,465,587,993,995"
-TCP_OUT = "20,21,22,25,53,80,110,113,443"
-
-# Listteger contra ataques de bruteforce FTP
+# Proteger contra ataques de bruteforce FTP
 LF_FTPD = "5"
 
 # Proteger contra ataques de bruteforce POP3
