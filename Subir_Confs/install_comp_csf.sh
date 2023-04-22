ba65d670e6899d854c01b2dd7d00ee18941cdc87
root <thalles_tsc@hotmail.com>
2023-04-21 23:47:24 -0300

    Atualização

--- Subir_Confs/install_comp_csf.sh
+++ Subir_Confs/install_comp_csf.sh
@@ -1,1 +1,3 @@
 sudo apt-get install sendmail dnsutils unzip libio-socket-ssl-perl libcrypt-ssleay-perl git perl iptables libnet-libidn-perl libio-socket-inet6-perl libsocket6-perl -y
+cd /usr/src/csf/
+sudo ./install.sh
