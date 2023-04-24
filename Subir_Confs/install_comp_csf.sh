76c314eabfb15f194a83d92a69cb87f2e17f4a9c
root <thalles_tsc@hotmail.com>
2023-04-24 16:36:44 -0300

    Atualização

--- Subir_Confs/install_comp_csf.sh
+++ Subir_Confs/install_comp_csf.sh
@@ -1,3 +1,5 @@
 sudo apt-get install sendmail dnsutils unzip libio-socket-ssl-perl libcrypt-ssleay-perl git perl iptables libnet-libidn-perl libio-socket-inet6-perl libsocket6-perl -y
 cd /usr/src/csf/
 sudo ./install.sh
+sudo systemctl enable csf
+sudo systemctl restart csf
