e78d521c1f04b000eada849b6196eed6d3fef4a9
root <thalles_tsc@hotmail.com>
2023-04-25 18:06:47 -0300

    Atualização

--- Subir_Confs/install_comp_csf.sh
+++ Subir_Confs/install_comp_csf.sh
@@ -5,3 +5,14 @@ sudo systemctl enable csf
 sudo systemctl start csf
 sudo apt autoremove -y
 sudo apt autoclean
+
+sudo csf -a 192.168.2.10 Rancher
+sudo csf -a 192.168.2.11 Primeiro agente do Rancher
+sudo csf -a 192.168.2.12 Segundo agente do Rancher
+sudo csf -a 192.168.2.13 Terceiro agente do Rancher
+sudo csf -a 192.168.2.50 Servidor do ProxMox
+sudo csf -a 192.168.2.51 Servidor do ProxMox
+sudo csf -a 192.168.2.200 Servidor de DNS - NS1
+sudo csf -a 192.168.2.201 Servidor de DNS - NS2
+sudo csf -a 192.168.2.203 Servidor de Torrent
+sudo csf -a 192.168.2.150 Máquina terminal para comendos
