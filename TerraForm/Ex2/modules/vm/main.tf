cd8b1ef99fec16c934c0c721eb3f9ab16c4b4bee
root <thalles_tsc@hotmail.com>
2023-04-27 16:58:17 -0300

    Atualização

--- TerraForm/Ex2/modules/vm/main.tf
+++ TerraForm/Ex2/modules/vm/main.tf
@@ -62,4 +62,28 @@ resource "proxmox_vm_qemu" "virtual_machines" {
       working_dir = "../ansible/"
       command = "ansible-playbook -u ${each.value.ssh_user} --key-file ${var.ssh_keys["priv"]} -i hosts.yaml provision.yaml"
   }
+
+  # Padrinização das máquinas DNS-NS1
+  provisioner "local-exec" {
+      working_dir = "../ansible/"
+      command = "ansible-playbook -u ${each.value.ssh_user} --key-file ${var.ssh_keys["priv"]} -i indnsns1.yaml dnsns1.yaml"
+  }
+
+  # Padrinização das máquinas DNS-NS2
+  provisioner "local-exec" {
+      working_dir = "../ansible/"
+      command = "ansible-playbook -u ${each.value.ssh_user} --key-file ${var.ssh_keys["priv"]} -i indnsns2.yaml dnsns2.yaml"
+  }
+
+  # Padrinização das máquinas AGENTES
+  provisioner "local-exec" {
+      working_dir = "../ansible/"
+      command = "ansible-playbook -u ${each.value.ssh_user} --key-file ${var.ssh_keys["priv"]} -i agentes.yaml pb_agentes.yaml"
+  }
+
+  # Padrinização das máquinas RANCHER
+  provisioner "local-exec" {
+      working_dir = "../ansible/"
+      command = "ansible-playbook -u ${each.value.ssh_user} --key-file ${var.ssh_keys["priv"]} -i rancher.yaml pb_rancher.yaml"
+  }
 }
