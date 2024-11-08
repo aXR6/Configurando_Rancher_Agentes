442a6712f9d1f0085d58f397f38543232db9e7ff
ThallesCanela <thalles_tsc@hotmail.com>
2024-11-08 10:44:58 -0300

    Criação de grupo de acesso, remoção de solicitação de senha para o grupo recem criado

--- OpenTofu/Gerando_Img/dns.sh
+++ OpenTofu/Gerando_Img/dns.sh
@@ -36,4 +36,22 @@ sudo apt-get autoclean || { echo "Erro ao realizar autoclean."; exit 1; }
 echo "Limpando cache de pacotes..."
 sudo apt-get clean || { echo "Erro ao limpar cache de pacotes."; exit 1; }
 
+# Verifica e cria o grupo 'wheel' se não existir
+echo "Verificando a existência do grupo 'wheel'..."
+if ! getent group wheel > /dev/null; then
+  echo "Grupo 'wheel' não encontrado. Criando grupo 'wheel'..."
+  sudo groupadd wheel || { echo "Erro ao criar o grupo 'wheel'."; exit 1; }
+else
+  echo "Grupo 'wheel' já existe."
+fi
+
+# Permitir que o grupo 'wheel' tenha sudo sem senha
+echo "Configurando sudo sem senha para o grupo 'wheel'..."
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/wheel-nopasswd > /dev/null || { echo "Erro ao configurar sudo sem senha para o grupo 'wheel'."; exit 1; }
+
+# Adicionar o usuário 'notroot' ao grupo 'wheel'
+USER_TO_ADD="notroot"
+echo "Adicionando o usuário $USER_TO_ADD ao grupo 'wheel'..."
+sudo usermod -aG wheel "$USER_TO_ADD" || { echo "Erro ao adicionar o usuário $USER_TO_ADD ao grupo 'wheel'."; exit 1; }
+
 echo "Processo concluído com sucesso."
