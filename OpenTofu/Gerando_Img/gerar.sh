f3721414e09a015f42e16709efe4dc4e3cd2a491
ThallesCanela <thalles_tsc@hotmail.com>
2024-11-09 00:25:07 -0300

    Adição da funcionalidade de cloud-init do rancher com as devidas alterações. Modificações no menu.

--- OpenTofu/Gerando_Img/gerar.sh
+++ OpenTofu/Gerando_Img/gerar.sh
@@ -3,12 +3,15 @@
 # Variáveis globais fixas
 IMAGE_NAME="debian-12-backports-genericcloud-amd64-daily.qcow2"
 VOLUME_NAME="local-lvm"
-SSHD_CONFIG_FILE="sshd_config"  # Caminho do arquivo local do sshd_config
-PUBLIC_KEY_FILE="id_rsa.pub"    # Caminho do arquivo local da chave pública
-PRIVATE_KEY_FILE="id_rsa"       # Caminho do arquivo local da chave privada
-SCRIPT_FILE="update.sh"         # Nome do arquivo de script a ser executado
+SSHD_CONFIG_FILE="sshd_config"      # Caminho do arquivo local do sshd_config
+PUBLIC_KEY_FILE="id_rsa.pub"        # Caminho do arquivo local da chave pública
+PRIVATE_KEY_FILE="id_rsa"           # Caminho do arquivo local da chave privada
 
-SCRIPT_FILE_dns="dns.sh"       	# Nome do arquivo de script a ser executado
+SCRIPT_FILE="comum.sh"              # Nome do arquivo de script a ser executado
+SCRIPT_FILE_rancher="rancher.sh"    # Nome do arquivo de script a ser executado
+SCRIPT_FILE_agentes="agentes.sh"    # Nome do arquivo de script a ser executado
+SCRIPT_FILE_dns="dns.sh"            # Nome do arquivo de script a ser executado
+
 Dir_dnsns1="/home/img/dnsns1"
 Dir_dnsns2="/home/img/dnsns2"
 
@@ -67,9 +70,137 @@ configure_image_params() {
   esac
 }
 
-# Função 1
+# Função VM RANCHER
 # Função para criar o template no Proxmox
-create_template() {
+create_template_rancher() {
+  echo "Criando template $TEMPLATE_NAME com ID $VM_ID..."
+
+  echo "Remoção de uma possível VM com o mesmo ID"
+  qm destroy "$VM_ID" --purge &> /dev/null || echo "Nenhuma VM com ID $VM_ID encontrada para remover."
+
+  # Instalação do qemu-guest-agent, SSH, configuração do sshd_config e cópia das chaves SSH
+  echo "Instalando qemu-guest-agent, SSH e copiando configuração sshd_config e chaves SSH na imagem..."
+  virt-customize -a "$IMAGE_NAME" --install qemu-guest-agent,openssh-server || error_exit "Falha ao instalar qemu-guest-agent e SSH."
+  virt-customize -a "$IMAGE_NAME" --copy-in "$SSHD_CONFIG_FILE":/etc/ssh || error_exit "Falha ao copiar sshd_config."
+
+  echo "Criação do diretório ~/.ssh"
+  virt-customize -a "$IMAGE_NAME" --run-command 'mkdir -p /root/.ssh && chmod 700 /root/.ssh' || error_exit "Falha ao criar diretório ~/.ssh."
+
+  echo "Criação do diretório /home/notroot/.ssh/"
+  virt-customize -a "$IMAGE_NAME" --run-command 'mkdir -p /home/notroot/.ssh/ && chmod 700 /home/notroot/.ssh/' || error_exit "Falha ao criar diretório /home/notroot/.ssh/"
+
+  echo "Cópia das chaves pública e privada - /root/.ssh/"
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PUBLIC_KEY_FILE":/root/.ssh/ || error_exit "Falha ao copiar chave pública."
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PRIVATE_KEY_FILE":/root/.ssh/ || error_exit "Falha ao copiar chave privada."
+
+    echo "Ajuste de permissões das chaves - /root/.ssh/"
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 600 /root/.ssh/id_rsa' || error_exit "Falha ao ajustar permissões das chaves SSH."
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 644 /root/.ssh/id_rsa.pub' || error_exit "Falha ao ajustar permissões das chaves SSH."
+
+  echo "Cópia das chaves pública e privada - /home/notroot/.ssh/"
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PUBLIC_KEY_FILE":/home/notroot/.ssh/ || error_exit "Falha ao copiar chave pública."
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PRIVATE_KEY_FILE":/home/notroot/.ssh/ || error_exit "Falha ao copiar chave privada."
+
+    echo "Ajuste de permissões das chaves - /home/notroot/.ssh/"
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 600 /home/notroot/.ssh/id_rsa' || error_exit "Falha ao ajustar permissões das chaves SSH."
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 644 /home/notroot/.ssh/id_rsa.pub' || error_exit "Falha ao ajustar permissões das chaves SSH."
+
+  echo "Copiar o script para a imagem"
+  virt-customize -a "$IMAGE_NAME" --copy-in "$SCRIPT_FILE_rancher":/usr/local/bin/ || error_exit "Falha ao copiar o script."
+
+  echo "Tornar o script executável"
+  virt-customize -a "$IMAGE_NAME" --run-command "chmod +x /usr/local/bin/$SCRIPT_FILE_rancher" || error_exit "Falha ao tornar o script executável."
+
+  echo "Criar o arquivo de serviço systemd"
+  virt-customize -a "$IMAGE_NAME" --run-command "echo -e '[Unit]\nDescription=Executar script customizado no boot\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/$SCRIPT_FILE\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/custom-script.service" || error_exit "Falha ao criar o arquivo de serviço systemd."
+
+  echo "Habilitar o serviço para iniciar no boot"
+  virt-customize -a "$IMAGE_NAME" --run-command 'systemctl enable custom-script.service' || error_exit "Falha ao habilitar o serviço no systemd."
+
+  echo "Criação da VM no Proxmox"
+  qm create "$VM_ID" --name "$TEMPLATE_NAME" --memory "$MEMORY" --cores "$CORES" --net0 virtio,bridge=vmbr0 || error_exit "Falha ao criar VM."
+  qm importdisk "$VM_ID" "$IMAGE_NAME" "$VOLUME_NAME" || error_exit "Falha ao importar disco."
+  qm set "$VM_ID" --scsihw virtio-scsi-single --scsi0 "$VOLUME_NAME:vm-$VM_ID-disk-0" || error_exit "Falha ao configurar o SCSI."
+  qm set "$VM_ID" --agent enabled=1,fstrim_cloned_disks=1 || error_exit "Falha ao configurar o agente."
+
+  echo "Configuração do Cloud-Init Disk, boot e vídeo padrão"
+  qm set "$VM_ID" --ide2 "$VOLUME_NAME:cloudinit" || error_exit "Falha ao configurar Cloud-Init."
+  qm set "$VM_ID" --boot c --bootdisk scsi0 || error_exit "Falha ao configurar o boot."
+  qm set "$VM_ID" --serial0 socket --vga std || error_exit "Falha ao configurar VGA padrão."
+
+  echo "Convertendo para template"
+  qm template "$VM_ID" || error_exit "Falha ao converter para template."
+
+  echo "Template $TEMPLATE_NAME criado com sucesso."
+}
+
+# Função VM AGENTES
+# Função para criar o template no Proxmox
+create_template_agentes() {
+  echo "Criando template $TEMPLATE_NAME com ID $VM_ID..."
+
+  echo "Remoção de uma possível VM com o mesmo ID"
+  qm destroy "$VM_ID" --purge &> /dev/null || echo "Nenhuma VM com ID $VM_ID encontrada para remover."
+
+  # Instalação do qemu-guest-agent, SSH, configuração do sshd_config e cópia das chaves SSH
+  echo "Instalando qemu-guest-agent, SSH e copiando configuração sshd_config e chaves SSH na imagem..."
+  virt-customize -a "$IMAGE_NAME" --install qemu-guest-agent,openssh-server || error_exit "Falha ao instalar qemu-guest-agent e SSH."
+  virt-customize -a "$IMAGE_NAME" --copy-in "$SSHD_CONFIG_FILE":/etc/ssh || error_exit "Falha ao copiar sshd_config."
+
+  echo "Criação do diretório ~/.ssh"
+  virt-customize -a "$IMAGE_NAME" --run-command 'mkdir -p /root/.ssh && chmod 700 /root/.ssh' || error_exit "Falha ao criar diretório ~/.ssh."
+
+  echo "Criação do diretório /home/notroot/.ssh/"
+  virt-customize -a "$IMAGE_NAME" --run-command 'mkdir -p /home/notroot/.ssh/ && chmod 700 /home/notroot/.ssh/' || error_exit "Falha ao criar diretório /home/notroot/.ssh/"
+
+  echo "Cópia das chaves pública e privada - /root/.ssh/"
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PUBLIC_KEY_FILE":/root/.ssh/ || error_exit "Falha ao copiar chave pública."
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PRIVATE_KEY_FILE":/root/.ssh/ || error_exit "Falha ao copiar chave privada."
+
+    echo "Ajuste de permissões das chaves - /root/.ssh/"
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 600 /root/.ssh/id_rsa' || error_exit "Falha ao ajustar permissões das chaves SSH."
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 644 /root/.ssh/id_rsa.pub' || error_exit "Falha ao ajustar permissões das chaves SSH."
+
+  echo "Cópia das chaves pública e privada - /home/notroot/.ssh/"
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PUBLIC_KEY_FILE":/home/notroot/.ssh/ || error_exit "Falha ao copiar chave pública."
+  virt-customize -a "$IMAGE_NAME" --copy-in "$PRIVATE_KEY_FILE":/home/notroot/.ssh/ || error_exit "Falha ao copiar chave privada."
+
+    echo "Ajuste de permissões das chaves - /home/notroot/.ssh/"
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 600 /home/notroot/.ssh/id_rsa' || error_exit "Falha ao ajustar permissões das chaves SSH."
+    virt-customize -a "$IMAGE_NAME" --run-command 'chmod 644 /home/notroot/.ssh/id_rsa.pub' || error_exit "Falha ao ajustar permissões das chaves SSH."
+
+  echo "Copiar o script para a imagem"
+  virt-customize -a "$IMAGE_NAME" --copy-in "$SCRIPT_FILE_agentes":/usr/local/bin/ || error_exit "Falha ao copiar o script."
+
+  echo "Tornar o script executável"
+  virt-customize -a "$IMAGE_NAME" --run-command "chmod +x /usr/local/bin/$SCRIPT_FILE_agentes" || error_exit "Falha ao tornar o script executável."
+
+  echo "Criar o arquivo de serviço systemd"
+  virt-customize -a "$IMAGE_NAME" --run-command "echo -e '[Unit]\nDescription=Executar script customizado no boot\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/$SCRIPT_FILE\n\n[Install]\nWantedBy=multi-user.target' > /etc/systemd/system/custom-script.service" || error_exit "Falha ao criar o arquivo de serviço systemd."
+
+  echo "Habilitar o serviço para iniciar no boot"
+  virt-customize -a "$IMAGE_NAME" --run-command 'systemctl enable custom-script.service' || error_exit "Falha ao habilitar o serviço no systemd."
+
+  echo "Criação da VM no Proxmox"
+  qm create "$VM_ID" --name "$TEMPLATE_NAME" --memory "$MEMORY" --cores "$CORES" --net0 virtio,bridge=vmbr0 || error_exit "Falha ao criar VM."
+  qm importdisk "$VM_ID" "$IMAGE_NAME" "$VOLUME_NAME" || error_exit "Falha ao importar disco."
+  qm set "$VM_ID" --scsihw virtio-scsi-single --scsi0 "$VOLUME_NAME:vm-$VM_ID-disk-0" || error_exit "Falha ao configurar o SCSI."
+  qm set "$VM_ID" --agent enabled=1,fstrim_cloned_disks=1 || error_exit "Falha ao configurar o agente."
+
+  echo "Configuração do Cloud-Init Disk, boot e vídeo padrão"
+  qm set "$VM_ID" --ide2 "$VOLUME_NAME:cloudinit" || error_exit "Falha ao configurar Cloud-Init."
+  qm set "$VM_ID" --boot c --bootdisk scsi0 || error_exit "Falha ao configurar o boot."
+  qm set "$VM_ID" --serial0 socket --vga std || error_exit "Falha ao configurar VGA padrão."
+
+  echo "Convertendo para template"
+  qm template "$VM_ID" || error_exit "Falha ao converter para template."
+
+  echo "Template $TEMPLATE_NAME criado com sucesso."
+}
+
+# Função VM COMUM
+# Função para criar o template no Proxmox
+create_template_comum() {
   echo "Criando template $TEMPLATE_NAME com ID $VM_ID..."
 
   echo "Remoção de uma possível VM com o mesmo ID"
@@ -275,9 +406,17 @@ while true; do
     read -p "Escolha uma opção: " opcao
 
     case $opcao in
-        1|2|3)
+        1)
             configure_image_params "$opcao"
-            create_template
+            create_template_rancher
+            ;;
+        2)
+            configure_image_params "$opcao"
+            create_template_agentes
+            ;;
+        3)
+            configure_image_params "$opcao"
+            create_template_comum
             ;;
         4)
             configure_image_params "$opcao"
