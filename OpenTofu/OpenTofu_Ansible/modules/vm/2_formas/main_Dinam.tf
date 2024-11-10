ae1dc60c57a8152f5da63fbe1a8f69c18d19276d
ThallesCanela <thalles_tsc@hotmail.com>
2024-11-10 14:26:01 -0300

    Foi criado umas pasta (2_formas) para depositar 2 formas de criar as VMS. Ambas estão executando normalmente.

---
+++ OpenTofu/OpenTofu_Ansible/modules/vm/2_formas/main_Dinam.tf
@@ -0,0 +1,107 @@
+# Recurso para criação das VMs sem provisionamento SSH imediato
+resource "proxmox_vm_qemu" "virtual_machine" {
+  for_each         = var.virtual_machines
+  name             = each.value.name
+  onboot           = each.value.onboot
+  scsihw           = each.value.scsihw
+  qemu_os          = each.value.qemu_os
+  desc             = each.value.description
+  target_node      = each.value.target_node
+  os_type          = each.value.os_type
+  full_clone       = each.value.full_clone
+  clone            = each.value.template
+  vmid             = each.value.vmid
+  memory           = each.value.memory
+  sockets          = each.value.socket
+  cores            = each.value.cores
+  vcpus            = each.value.vcpus
+  cpu              = each.value.cpu
+  balloon          = each.value.balloon
+  numa             = each.value.numa
+  ssh_user         = each.value.ssh_user
+  sshkeys          = file(var.ssh_keys["pub"])
+  ciuser           = each.value.ssh_user        
+  cipassword       = each.value.cloud_init_pass
+  ipconfig0        = "ip=${each.value.ip_address}/24,gw=${each.value.gateway}"
+  automatic_reboot = each.value.automatic_reboot
+  hotplug          = each.value.hotplug
+  searchdomain     = each.value.searchdomain
+  nameserver       = each.value.nameserver
+  agent            = each.value.agent
+
+  disk {
+    type    = var.disk_type_ci
+    storage = var.storage_ci
+    slot    = var.slot_ci
+  }
+
+  disk {
+    storage = each.value.storage_dev
+    type    = each.value.disk_type
+    size    = each.value.storage
+    slot    = each.value.bootdisk
+  }
+
+  network {
+    bridge   = each.value.network_bridge_type
+    model    = each.value.network_model
+    mtu      = 0
+    macaddr  = each.value.mac_address
+    queues   = 0
+    rate     = 0
+    firewall = each.value.network_firewall
+  }
+}
+
+/*
+# Recurso para provisionamento e configuração via SSH após a criação das VMs
+resource "null_resource" "provision_vms" {
+  for_each = var.virtual_machines
+
+  depends_on = [proxmox_vm_qemu.virtual_machine] # Aguarda a criação de todas as VMs
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'notroot ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/notroot",
+      "sudo chmod 0440 /etc/sudoers.d/notroot"
+    ]
+  }
+  connection {
+    type     = "ssh"
+    host     = each.value.ip_address
+    user     = each.value.ssh_user
+    password = each.value.cloud_init_pass # Substitua pelo valor correspondente da senha
+    timeout  = "5m"
+  }
+
+  # Provisionamento inicial com Ansible para cada VM
+  provisioner "local-exec" {
+    working_dir = "../ansible/"
+    command     = "ansible-playbook -u ${each.value.ssh_user} -i hosts.yaml provision.yaml --extra-vars 'ansible_password=${each.value.cloud_init_pass}'"
+  }
+
+  # Provisionamento para DNS-NS1
+  provisioner "local-exec" {
+    working_dir = "../ansible/"
+    command     = "ansible-playbook -u ${each.value.ssh_user} -i indnsns1.yaml dnsns1.yaml --extra-vars 'ansible_password=${each.value.cloud_init_pass}'"
+  }
+
+  # Provisionamento para DNS-NS2
+  provisioner "local-exec" {
+    working_dir = "../ansible/"
+    command     = "ansible-playbook -u ${each.value.ssh_user} -i indnsns2.yaml dnsns2.yaml --extra-vars 'ansible_password=${each.value.cloud_init_pass}'"
+  }
+
+  # Provisionamento para AGENTES
+  provisioner "local-exec" {
+    working_dir = "../ansible/"
+    command     = "ansible-playbook -u ${each.value.ssh_user} -i agentes.yaml pb_agentes.yaml --extra-vars 'ansible_password=${each.value.cloud_init_pass}'"
+  }
+
+  # Provisionamento para RANCHER
+  provisioner "local-exec" {
+    working_dir = "../ansible/"
+    command     = "ansible-playbook -u ${each.value.ssh_user} -i rancher.yaml pb_rancher.yaml --extra-vars 'ansible_password=${each.value.cloud_init_pass}'"
+  }
+}
+*/
