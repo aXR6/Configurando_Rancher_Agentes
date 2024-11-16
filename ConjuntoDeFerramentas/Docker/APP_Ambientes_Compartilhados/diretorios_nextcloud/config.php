81c19746d3b41240ace01b615a152629ff7364cc
ThallesCanela <thalles_tsc@hotmail.com>
2024-11-15 23:19:03 -0300

    Organizando arquivos e diretorios

--- ConjuntoDeFerramentas/Docker/APP_Ambientes_Compartilhados/diretorios_nextcloud/config.php
+++ ConjuntoDeFerramentas/Docker/APP_Ambientes_Compartilhados/diretorios_nextcloud/config.php
@@ -1,7 +1,51 @@
-'memcache.local' => '\OC\Memcache\APCu',
-'memcache.locking' => '\OC\Memcache\Redis',
-'redis' => array(
-  'host' => 'redis',
-  'port' => 6379,
-  'password' => '36HBpD',
-),
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.locking' => '\\OC\\Memcache\\Redis', // Adicionado para Redis
+  'redis' => array( // Configurações do Redis
+    'host' => 'redis',
+    'port' => 6379,
+    'password' => '36HBpD',
+  ),
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'instanceid' => 'ocss15v3688k',
+  'passwordsalt' => 'eyir7LR+pIaNSITUt8EwRpx4y4z9WN',
+  'secret' => '6TmVk5lu5Qn/XAIPU0iyOAh2zwvhXCFIe3TYX1brD5uWxzQv',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.3.202',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '30.0.2.2',
+  'overwrite.cli.url' => 'http://192.168.3.202',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'nextcloud_db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '36HBpD61l)Lj',
+  'installed' => true,
+  'maintenance' => false,
+  'backgroundjobs_mode' => 'cron',
+  'maintenance_window_start' => '15:00',
+  'default_phone_region' => 'BR',
+  'files_external_allow_new_local' => true,
+);
