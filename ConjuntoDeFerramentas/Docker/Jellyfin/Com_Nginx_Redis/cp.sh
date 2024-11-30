80bd5aa6e676630b794d123fef0e9ce058454a42
ThallesCanela <thalles_tsc@hotmail.com>
2024-11-30 17:33:34 -0300

    Adaptações nos docker-compose do Jellyfin e no transmission

--- ConjuntoDeFerramentas/Docker/Jellyfin/Com_Nginx_Redis/cp.sh
+++ ConjuntoDeFerramentas/Docker/Jellyfin/Com_Nginx_Redis/cp.sh
@@ -1,5 +1,5 @@
-rm /home/stream/nginx/nginx.conf && nano /home/stream/nginx/nginx.conf
-rm /home/stream/nginx/conf.d/default.conf && nano /home/stream/nginx/conf.d/default.conf
+rm /home/stream/nginx/nginx.conf
+rm /home/stream/nginx/conf.d/default.conf
 
 cp nginx/conf.d/default.conf /home/stream/nginx/conf.d/
 cp nginx/conf.d/nginx.conf /home/stream/nginx/
