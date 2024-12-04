194c74b2e54b239113372e1188ef0ac02a7272dc
ThallesCanela <thalles_tsc@hotmail.com>
2024-12-04 12:02:25 -0300

    Versão estavel e funcional - Traefik como Proxy

--- ConjuntoDeFerramentas/Docker/Jellyfin/Com_Nginx_Redis/cp.sh
+++
@@ -1,8 +0,0 @@
-rm /home/stream/nginx/nginx.conf
-rm /home/stream/nginx/conf.d/default.conf
-
-cp nginx/conf.d/default.conf /home/stream/nginx/conf.d/
-cp nginx/conf.d/nginx.conf /home/stream/nginx/
-
-ls -la /home/stream/nginx/conf.d/
-ls -la /home/stream/nginx/
